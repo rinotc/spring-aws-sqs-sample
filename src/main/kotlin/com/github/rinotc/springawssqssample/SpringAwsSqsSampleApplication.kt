794d1063c29d444adec189254e2cf7460db84d22
toshinori.chiba <toshinori.chiba.p@gmail.com>
2024-06-01 12:17:56 +0900

    initial commit

---
+++ src/main/kotlin/com/github/rinotc/springawssqssample/SpringAwsSqsSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.github.rinotc.springawssqssample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAwsSqsSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAwsSqsSampleApplication>(*args)
+}
